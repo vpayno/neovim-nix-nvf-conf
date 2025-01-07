5bd4f78c661003bfdff312bc7eb888d716fae380
Victor Payno <vpayno@users.noreply.github.com>
2025-01-06 23:01:34 -0800

    nvim: add initial lsp config

--- modules/nvim-config.nix
+++ modules/nvim-config.nix
@@ -4,4 +4,15 @@
     name = "gruvbox";
     style = "dark";
   };
+
+  languages = {
+    enableLSP = true;
+    enableTreesitter = true;
+  };
+
+  lsp = {
+    enable = true;
+    lspSignature.enable = true;
+    lsplines.enable = true;
+  };
 }
