2e69c8fc1a92a8b3f59d9cb71893070eafc9973d
Victor Payno <vpayno@users.noreply.github.com>
2025-01-18 22:01:08 -0800

    nix: use forAllSystems for formatter

--- flake.nix
+++ flake.nix
@@ -1,30 +1,37 @@
+# flake.nix
 {
   inputs = {
     nixpkgs.url = "github:nixOS/nixpkgs/nixos-24.11";
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nvf,
-      ...
-    }:
-    {
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+  outputs = {
+    self,
+    nixpkgs,
+    nvf,
+    ...
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
 
-      packages.x86_64-linux.default =
-        (nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [ ./nvf-configuration.nix ];
-        }).neovim;
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
 
-      nixosConfiguration.nixos = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./configuration.nix
-          nvf.nixosModules.default
-        ];
-      };
+    packages.x86_64-linux.default =
+      (nvf.lib.neovimConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [./nvf-configuration.nix];
+      })
+      .neovim;
+
+    nixosConfiguration.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+        nvf.nixosModules.default
+      ];
     };
+  };
 }
